6a31e90e08373202f077abc07aa36686c84b307f
洛维奇 <luoweiqiang9527@163.com>
2025-11-24 14:49:43 +0800

    简单的接口访问

--- src/main/java/spring/ai/example/spring_ai_demo/SpringAiDemoApplication.java
+++ src/main/java/spring/ai/example/spring_ai_demo/SpringAiDemoApplication.java
@@ -1,13 +1,27 @@
 package spring.ai.example.spring_ai_demo;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class SpringAiDemoApplication {
+    private ChatClient client ;
+    public SpringAiDemoApplication(ChatClient.Builder builder) {
+        this.client = builder.build();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiDemoApplication.class, args);
 	}
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("prompt") String prompt) {
+        return client.prompt(prompt).call().content();
+    }
+
+
 
 }
